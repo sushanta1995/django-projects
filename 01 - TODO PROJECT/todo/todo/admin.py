7687127ddcfb06390cb0b7daa48c45f6bbe822a8
sushanta1995 <sahoosushanta1995@gmail.com>
2025-07-07 12:05:32 +0530

    Delete 01 - TODO PROJECT directory

--- 01 - TODO PROJECT/todo/todo/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from todo.models import TODOO
-
-admin.site.register(TODOO)
--- 01 - TODO PROJECT/todo/todo/static/js/todo.js
+++
@@ -1,3 +0,0 @@
-function changeColor(clickedDiv) {
-    clickedDiv.style.backgroundColor = 'green'; // Change the color of the clicked div to red
-  }
